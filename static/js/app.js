acb7ef521adfcf3bbc7d67199a6fba0e2bc7002d
cheriemosher <mosher.cherie@hushmail.com>
2020-06-19 00:05:41 -0700

    so many updates.  Mostly, changed the thumbnails to modal images

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,9 @@
-$('.dropdown-toggle').dropdown()
+function switchStyle() {
+    if (document.getElementById('styleSwitch').checked) {
+      document.getElementById('gallery').classList.add("custom");
+      document.getElementById('exampleModal').classList.add("custom");
+    } else {
+      document.getElementById('gallery').classList.remove("custom");
+      document.getElementById('exampleModal').classList.remove("custom");
+    }
+  }
